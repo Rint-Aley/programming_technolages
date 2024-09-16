e126098325267113a614c532a8c87bff9207d6f2
Rint-Aley <rint.aley@gmail.com>
2024-09-16 10:16:09 +0300

    Add calculation of volume of the cylinder

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::io;
 fn main() {
-    println!("Input two numbers separated with space");
+    println!("Input radius and height of cylinder");
     let mut numbers = String::new();
     io::stdin().read_line(&mut numbers).unwrap();
     let numbers: Vec<i32> = numbers[..numbers.len() - 2]
         .split(' ')
         .map(|num: &str| num.parse().unwrap())
         .collect();
-    println!("Your numbers are: {} {}", numbers[0], numbers[1]);
+    println!("The voulume of cylinder is {}", get_cylinder_volume(numbers[0], numbers[1]));
+}
+
+fn get_cylinder_volume(radius: i32, height: i32) -> f32 {
+    return std::f32::consts::PI * (radius.pow(2) * height) as f32;
 }
